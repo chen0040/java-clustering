a2b931db11cb1203b86e47b0bdfd9a3899cc4ad0
xschen <xs0040@gmail.com>
2017-05-30 09:42:28 +0800

    k-means unit test

--- src/test/java/com/github/chen0040/clustering/kmeans/KMeansUnitTest.java
+++ src/test/java/com/github/chen0040/clustering/kmeans/KMeansUnitTest.java
@@ -1,7 +1,9 @@
 package com.github.chen0040.clustering.kmeans;
 
 
+import com.github.chen0040.clustering.utils.FileUtils;
 import com.github.chen0040.data.frame.DataFrame;
+import com.github.chen0040.data.frame.DataRow;
 import com.github.chen0040.data.image.ImageDataFrameFactory;
 import org.testng.annotations.Test;
 
@@ -22,5 +24,46 @@ public class KMeansUnitTest {
 
    private static Random rand = new Random();
 
-  
+   @Test
+   public void test_image_segmentation(){
+      BufferedImage img=null;
+      try{
+         img= ImageIO.read(FileUtils.getResource("1.jpg"));
+      }catch(IOException ie)
+      {
+         ie.printStackTrace();
+      }
+
+      DataFrame data = ImageDataFrameFactory.dataFrame(img);
+
+      KMeans cluster = new KMeans();
+      cluster.setMaxIters(200);
+      cluster.setClusterCount(25);
+
+      cluster.fitAndTransform(data);
+
+      List<Integer> classColors = new ArrayList<>();
+      for(int i=0; i < 5; ++i){
+         for(int j=0; j < 5; ++j){
+            classColors.add(ImageDataFrameFactory.get_rgb(255, rand.nextInt(255), rand.nextInt(255), rand.nextInt(255)));
+         }
+      }
+
+      BufferedImage class_img = new BufferedImage(img.getWidth(), img.getHeight(), img.getType());
+      for(int x=0; x < img.getWidth(); x++)
+      {
+         for(int y=0; y < img.getHeight(); y++)
+         {
+            int rgb = img.getRGB(x, y);
+
+            DataRow tuple = ImageDataFrameFactory.getPixelTuple(data, rgb);
+
+            int clusterIndex = cluster.transform(tuple);
+
+            rgb = classColors.get(clusterIndex);
+
+            class_img.setRGB(x, y, rgb);
+         }
+      }
+   }
 }
